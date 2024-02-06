ebfdab12c1dd25d04e68ba36c52335206db914c0
ChristopherNapoles-01 <christophernapoles35@gmail.com>
2024-02-06 23:29:02 +0800

    changed default connection

--- config/database.php
+++ config/database.php
@@ -96,7 +96,7 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('DB_DSN'),
-            'database' => env('DB_DATABASE', 'homestead'),
+            'database' => env('DB_DATABASE', 'crud_db'),
         ],
 
     ],
